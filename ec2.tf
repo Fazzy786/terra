6128614f15fe5359899de8e3d7b81dc95e48a9c1
Fazzy786 <92389875+Fazzy786@users.noreply.github.com>
2022-01-06 10:35:41 +0530

    update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_instance" "server1"
-{
+resource "aws_instance" "server1" {
   ami           = "ami-052cef05d01020f1d"
   instance_type = "t2.micro"
 }
